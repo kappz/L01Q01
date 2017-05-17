82d3f9c002121f660801622176880f6e575dfb90
kappz <addcolor117@gmail.com>
2017-05-17 11:43:08 -0400

    final version

--- L01Q01/L01Q01.cpp
+++ L01Q01/L01Q01.cpp
@@ -1,14 +1,10 @@
 /*
 Author: Peter O'Donohue
 Creation Date: 05/15/17
-Modification Date: 05/15/17
-Purpose: Practice with 2D arrays
+Modification Date: 05/17/17
+Description: this program searches the array given below and returns to total
+amount of even elements in the array
 */
-
-// L01Q01.cpp
-// from lab 01
-// this program searches the array given below and returns to total
-// amount of even elements in the array
 
 #include <iostream>
 using namespace std;
@@ -41,14 +37,15 @@ int main()
 	return 1;
 }
 
-/*
-Author: Peter O'Donohue
-Creation Date: 05/15/17
-Modification Date: 05/15/17
-Purpose: display greeting message
-*/
+// function definitions
+
 void greetingMessage(int nums[MAX_ROWS][MAX_COLUMNS], int length, int width)
 {
+	// PRE: the void function has been initialized, and the length and width specified
+	// POST: both greeting message and array has been printed to user
+	// control passes back to the main function
+
+	// print greeting message
 	cout << "Hello, this program searches the array below " << endl
 		 << "and returns the number of even elements" << endl
 		 << "contained within it." << endl << endl
@@ -62,43 +59,43 @@ void greetingMessage(int nums[MAX_ROWS][MAX_COLUMNS], int length, int width)
 		{
 			cout << nums[i][j];
 			if (j == 1)
-				cout << "} ";
+				cout << "}";
 			else
-				cout << " , ";
+				cout << ", ";
 		}
 		if (i < length - 1)
 			cout << ", ";
 	}
-	cout << "}" << endl << endl;
+	cout << " }" << endl << endl;
 	return;
 }
 
-/*
-Author: Peter O'Donohue
-Creation Date: 05/15/17
-Modification Date: 05/15/17
-Purpose: exit message
-*/
 void exitMessage()
 {
+	// PRE: void function 'exitMessage' has been initialized
+	// POST: exit message has been printed and control passes back to main function
+
+	// print exit message
 	cout << "Thanks for using my program, goodbye." << endl;
 }
 
-/*
-Author: Peter O'Donohue
-Creation Date: 05/15/17
-Modification Date: 05/15/17
-Purpose: compute number of even elements
-*/
 int findEvenElements(int nums[MAX_ROWS][MAX_COLUMNS], int length, int width)
 {
-	int tempEvenAmount = 0;
+	// PRE: the integers 'length', 'width', and the array 'findEvenElements' 
+	// has been initialized
+	// POST: the array has been searched and the amount of even integers have
+	// been calculated. Control passes back to the main function after the value
+	// 'tempEvenAmount' has been returned to the main function.
+
+	int tempEvenAmount = 0; // stores amount of even integers
+
+	// search array
 	for (int i = 0; i < length; ++i)
 	{
 		for (int j = 0; j < width; ++j)
 		{
-			if ((nums[i][j] % 2 == 0) && (nums[i][j] != 0))
-				++tempEvenAmount;
+			if ((nums[i][j] % 2 == 0) && (nums[i][j] != 0)) // if integer is even
+				++tempEvenAmount; // increment 
 		}
 	}
 	return tempEvenAmount;
